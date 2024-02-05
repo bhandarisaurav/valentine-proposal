88d96462549dd6e14f73b307d877dfe456eab1ae
bhandarisaurav <saurav.bhandari012@gmail.com>
2024-02-04 23:10:58 -0500

    :sparkles: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ValentineCard from './ValentineCard'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ValentineCard />
+    </>
+  )
+}
+
+export default App
