2e877a32c91551e184810f583f68ba6329a0bf1a
bhandarisaurav <saurav.bhandari012@gmail.com>
2024-02-04 23:17:31 -0500

    :sparkles: Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import ValentineCard from './ValentineCard'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
